3ca7b053d60e620b3b75c63b11e0632466e88522
alessandro <alessandro.massarella@gmail.com>
2020-10-26 20:01:00 +0100

    Fizz and Buzz

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 // ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-// modulo multiplo di 3 = num % 3 = 0
-// modulo multiplo di 5 = num % 5 = 0
+// modulo multiplo di 3 = num % 3 === 0
+// modulo multiplo di 5 = num % 5 === 0
 
 // creo un array vuoto di lunghezza 100
 var numeri = new Array(100);
@@ -12,5 +12,10 @@ var numeri = new Array(100);
 // inserisco i numeri nell'array
 for (var i = 0; i < 100; i++) {
   numeri[i] = i + 1;
+  if (numeri[i] % 3 === 0) {
+      document.getElementById('lista').innerHTML += ('<li>') + numeri[i] + (' Fizz') + ('</li>');
+  } else if (numeri[i] % 5 === 0) {
+          document.getElementById('lista').innerHTML += ('<li>') + numeri[i] + (' Buzz') + ('</li>');
+  } else
   document.getElementById('lista').innerHTML += ('<li>') + numeri[i] + ('</li>');
 }
